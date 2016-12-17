320bc204743db673ad9d71b468e979e6ce2cb308
Nickolay Kondratenko <devmarkup@gmail.com>
2016-12-17 12:30:10 +0200

    handle snake by serial usb

--- main.cpp
+++ main.cpp
@@ -1,48 +1,30 @@
 #include "snake.h"
 
-#define DIRECTION_UP 0
-#define DIRECTION_DOWN 1
-#define DIRECTION_LEFT 2
-#define DIRECTION_RIGHT 3
-
 snake gameSnake;
-bool i = false;
-uint8_t direction;
+uint8 input;
 
 void setup() {
-  randomSeed(analogRead(0));
   gameSnake.init();
 }
 
-void moveHorizontal() {
-  if (direction == DIRECTION_LEFT || random(0xffff) % 2) {
-    gameSnake.moveLeft();
-    direction = DIRECTION_LEFT;
-  }
-  else {
-    gameSnake.moveRight();
-    direction = DIRECTION_RIGHT;
-  }
-}
-
-void moveVertical() {
-  if (direction == DIRECTION_UP || random(0xffff) % 2) {
-    gameSnake.moveUp();
-    direction = DIRECTION_UP;
-  }
-  else {
-    gameSnake.moveDown();
-    direction = DIRECTION_DOWN;
-  }
-}
-
 void loop() {
-  if (random(4))
-    i = !i;
-
-  i ? moveHorizontal() : moveVertical();
-
-  delay(200);
+  while (SerialUSB.available()) {
+    input = SerialUSB.read();
+    switch (input) {
+      case 'h':
+        gameSnake.moveLeft();
+        break;
+      case 'j':
+        gameSnake.moveDown();
+        break;
+      case 'k':
+        gameSnake.moveUp();
+        break;
+      case 'l':
+        gameSnake.moveRight();
+        break;
+    }
+  }
 }
 
 __attribute__((constructor)) void premain() {
